5d3a5117301c86818f6c172f5af36e2de414b8fc
PuzhaikinMaxim <56976303+PuzhaikinMaxim@users.noreply.github.com>
2023-04-11 23:05:54 +0300

    Test STOMP Websocket implementation with authentication

---
+++ src/main/java/com/puj/stepfitnessapp/config/WebSocketAuthenticationConfig.java
@@ -0,0 +1,58 @@
+package com.puj.stepfitnessapp.config;
+
+import com.puj.stepfitnessapp.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+public class WebSocketAuthenticationConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final UserService userService;
+
+    @Autowired
+    public WebSocketAuthenticationConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(
+                        message,
+                        StompHeaderAccessor.class
+                );
+                if(StompCommand.CONNECT.equals(accessor.getCommand())){
+                    String token = accessor.getNativeHeader("token").stream().findFirst().orElse("");
+                    var optionalUser = userService.getUserByToken(token);
+                    if(optionalUser.isPresent()){
+                        var user = optionalUser.get();
+                        var authentication = new UsernamePasswordAuthenticationToken(
+                                user.getUsername(),
+                                user.getPassword(),
+                                user.getGrantedAuthorities()
+                        );
+                        accessor.setUser(authentication);
+                    }
+                    System.out.println(token);
+                }
+                return message;
+            }
+        });
+    }
+}
